5f186a1fba577dd5c7989f6665722e927310470b
MatRush <lvchaoshuai@qq.com>
2013-07-12 12:05:04 +0800

    Updated files

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require('fs');
+var infile = "index.html";
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(fs.readFileSync(infile).toString());
 });
 
 var port = process.env.PORT || 5000;
